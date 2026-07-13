d819b6d08d4c62181cb98f485e61881423745b90
amaturenoob <juvinilepolitcs@gmail.com>
2026-07-13 15:47:15 -0500

    test

--- scripts/gki/envsetup.sh
+++ scripts/gki/envsetup.sh
@@ -30,7 +30,8 @@ QCOM_QGKI_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}_QGKI.config
 QCOM_DEBUG_FRAG=${CONFIGS_DIR}/${PLATFORM_NAME}_debug.config
 
 # For user variant build merge debugfs.config fragment.
-if [ ${TARGET_BUILD_VARIANT} == "user" ]; then
+# shellcheck disable=SC1035
+if [ "${TARGET_BUILD_VARIANT}" ==  "user" ]; then
 	QCOM_DEBUG_FS_FRAG=`ls ${CONFIGS_DIR}/debugfs.config 2> /dev/null`
 else
 	QCOM_DEBUG_FS_FRAG=" "

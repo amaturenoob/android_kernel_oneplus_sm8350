2a3ec78266ecaed2edbad39f47286a63a6e6a645
amaturenoob <juvinilepolitcs@gmail.com>
2026-07-13 23:32:52 -0500

    test

--- tools/include/tools/config.h
+++ tools/include/tools/config.h
@@ -5,6 +5,8 @@
 /* Subset of include/linux/kconfig.h */
 
 #define __ARG_PLACEHOLDER_1 0,
+#define __ARG_PLACEHOLDER_y 0,
+#define __ARG_PLACEHOLDER_m 0,
 #define __take_second_arg(__ignored, val, ...) val
 
 /*
